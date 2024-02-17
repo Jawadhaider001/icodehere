1118c73227e61bf351ea08cd0aad5b6adcc7fdf4
Jawadhaider001 <jawadkaramat135@gmail.com>
2024-02-17 23:28:17 +0500

    get input form user

---
+++ 3 input form user.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter the value of a" <<endl;
+	cin>>a;
+	if(a==1)
+		{
+			cout<<"Your Pet is Cat"<<endl;
+		}
+	
+	else if (a==2)
+		{
+	
+			cout<<"Your Pet is Dog"<<endl;
+		}
+	else if (a==3)
+
+		{
+			cout<<"Your Pet is Dear"<<endl;
+		}
+
+	else
+		{
+			cout<<"Please enter the value between 1 to 3 "<<endl;
+		}
+	return 0;
+}
+
+
